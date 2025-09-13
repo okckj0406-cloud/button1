8a60f763a7ed57c15032a3628780f8f35e2744f3
Jaemin Chun <okckj0406@gmail.com>
2025-09-14 04:01:09 +0900

    edit id, name

--- button1.py
+++ button1.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Jamin Chun")
+b1 = Button(window, text="jamin Chun")
 b2 = Button(window, text="60222150")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
